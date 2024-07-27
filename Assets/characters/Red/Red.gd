eeb10c8680224ae6e2b6dbfcb732045de77ea2e2
Edwin Bera <edwinbera28@gmail.com>
2024-07-27 20:56:04 +0200

    ADD Player melee attack

--- Assets/characters/Red/Red.gd
+++ Assets/characters/Red/Red.gd
@@ -1,7 +1,10 @@
 extends Character
 
 onready var sprite = $Sprite
+onready var hitBox = $hitBox
 var is_jumping = false
+func _ready():
+	hitBox.set_disabled(true)
 func get_direction():
 	return Vector2(Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"), -1.0 if Input.is_action_just_pressed("ui_up") and is_on_floor() else 1.0)
 
@@ -24,15 +27,23 @@ func _physics_process(_delta):
 	animationHandler(direction)
 
 func animationHandler(direction):
-
-	if is_on_floor():
-		if direction.x != 0:
-			sprite.play("Run")
-			if direction.x > 0:
-				sprite.flip_h = true
+	if !(sprite.get_animation() == "Attack1" and !sprite.get_frame() == 6) :
+		if !hitBox.disabled:
+			hitBox.set_disabled(true)
+		if is_on_floor():
+			if direction.x != 0:
+				sprite.play("Run")
+				if direction.x > 0:
+					sprite.flip_h = true
+					hitBox.position.x = 20
+				else:
+					sprite.flip_h = false
+					hitBox.position.x = -17
 			else:
-				sprite.flip_h = false
-		else:
-			sprite.play("Idle")
-	elif direction.y < 0:
-		sprite.play("Jump")
+				sprite.play("Idle")
+		elif direction.y < 0:
+			sprite.play("Jump")
+func _input(event):
+	if event.is_action_pressed("attack"):
+		sprite.play("Attack1")
+		hitBox.set_disabled(false)
